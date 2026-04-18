0148dc84a83f4e4e426b57d46346cdf8112fcd0c
devmadcoin <baconasmrnoob@gmail.com>
2026-04-17 17:33:13 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import MadConfessions from "@/app/components/MadConfessions";
+import MadConfessions from "./components/MadConfessions";
 
 const LINKS = {
   telegram: "https://t.me/MadOfficalChannel",
